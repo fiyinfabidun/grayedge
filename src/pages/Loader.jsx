a78530ebf70be9d01e5c2448893fa9e4792a38d3
fiyinfabidun <fiyinfoluwafabidun@gmail.com>
2024-09-21 03:29:52 +0100

    new loader

--- src/pages/Loader.jsx
+++ src/pages/Loader.jsx
@@ -1,11 +1,11 @@
 // src/components/Loader.js
 import React from 'react';
-import { ClipLoader } from 'react-spinners';
+import { CircleLoader } from 'react-spinners';
 
 const Loader = ({ loading }) => {
   return (
     <div className="loader-container">
-      <ClipLoader color="#ffed92e6" loading={loading} size={80} />
+      <CircleLoader color="#ffed92e6" loading={loading} size={80} />
     </div>
   );
 };
