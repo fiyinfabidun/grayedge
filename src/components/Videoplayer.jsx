1c969973503c611975cb4cbe4ac2fd7703cf3755
fiyinfabidun <fiyinfoluwafabidun@gmail.com>
2024-09-21 03:56:06 +0100

    movie uploaded

--- src/components/Videoplayer.jsx
+++ src/components/Videoplayer.jsx
@@ -1,21 +1,13 @@
-import React, { useState } from 'react';
-import sampleVideo from '../images/DreamScapeSequence.mp4'; 
+import React  from 'react';
+ 
 
 const VideoPlayer = () => {
-  const [loading, setLoading] = useState(true);
-
-  const handleLoadedData = () => {
-    setLoading(false);
-  };
+ 
 
   return (
     <div className="video-container">
         <h1>THE STORY UNFOLDS</h1>
-      {loading && <div className="loader">Loading...</div>}
-      <video controls onLoadedData={handleLoadedData}>
-        <source src={sampleVideo} type="video/mp4" />
-        Your browser does not support the video tag.
-      </video>
+        <p>EPSIODE 1</p>
     </div>
   );
 };
